f958d3779d2c4ba04ebc9ab25efece8a11995707
Sanuro <39202300+Sanuro@users.noreply.github.com>
2021-01-09 02:01:59 -0500

    Add all relevant files

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("portfolio.html")
+
+@app.route("/rokuKeyboard")
+def rokuKeyboard():
+    return render_template("rokuKeyboard.html")
+
+@app.route("/dogFetchr")
+def dogFetchr():
+    return render_template("dogFetchr.html")
+
+@app.route("/libraryRedesign")
+def library():
+    return render_template("libraryRedesign.html")
+
+@app.route("/escapeRoom")
+def escapeRoom():
+    return render_template("escapeRoom.html")
+
+@app.route("/magazine")
+def magazine():
+    return render_template("magazine.html")
+
+@app.route("/leadership")
+def leadership():
+    return render_template("leadership.html")
